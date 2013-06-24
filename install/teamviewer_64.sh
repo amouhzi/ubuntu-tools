4445e5394129903b62dc377f6fa3ef7d8d36599f
anezi <github@anezi.net>
2013-06-24 22:44:14 +0300

    Update teamviewer_64.sh

--- install/teamviewer_64.sh
+++ install/teamviewer_64.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
 
+cd /tmp
+wget "http://download.teamviewer.com/download/teamviewer_linux_x64.deb"
+
+dpkg -iy "http://download.teamviewer.com/download/teamviewer_linux_x64.deb"
+
+apt-get install -fy
