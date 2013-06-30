55a93935954e464d7e90daa5e91ab76c2f3b6737
anezi <github@anezi.net>
2013-07-01 01:58:57 +0200

    Update oraclejava6.sh

--- install/oraclejava6.sh
+++ install/oraclejava6.sh
@@ -2,4 +2,5 @@
 
 sudo add-apt-repository ppa:webupd8team/java
 sudo apt-get update
-sudo apt-get install oracle-java6-set-default visualvm ttf-baekmuk ttf-unfonts ttf-unfonts-core ttf-kochi-gothic ttf-sazanami-gothic ttf-kochi-mincho ttf-sazanami-mincho ttf-arphic-uming
+sudo apt-get install oracle-java6-set-default visualvm ttf-baekmuk fonts-unfonts-extra ttf-unfonts-core \
+ttf-kochi-gothic ttf-sazanami-gothic ttf-kochi-mincho ttf-sazanami-mincho ttf-arphic-uming
