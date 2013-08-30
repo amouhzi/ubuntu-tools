98caedbf6cd8a525f268b942b0db96d421f049ef
Hassan Amouhzi <github@anezi.net>
2013-08-30 12:34:53 +0200

    Create eclipse.sh

---
+++ install/eclipse.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#aget ocacle-java8
+
+#cd /tmp
+
+#wget http://eclipse.mirror.kangaroot.net/technology/epp/downloads/release/kepler/R/eclipse-standard-kepler-R-linux-gtk-x86_64.tar.gz
+
+#tar -zxvf eclipse-standard-kepler-R-linux-gtk-x86_64.tar.gz
+
+#####
+
+# Not yet
