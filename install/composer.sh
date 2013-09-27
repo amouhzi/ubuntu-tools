20b624a520a690ddde2e81f9d5ded135c5bcf007
Hassan Amouhzi <github@anezi.net>
2013-09-27 22:17:39 +0200

    Update composer.sh

--- install/composer.sh
+++ install/composer.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 cd /tmp
-sudo apt-get install -y php5-cli curl
+sudo apt-get install -y php5-cli curl php5-json
 curl -sS https://getcomposer.org/installer | php
 sudo mv composer.phar /usr/local/bin/composer
