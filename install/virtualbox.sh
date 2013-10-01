5934c902cdab7fb00ef3b2676b28b0065600e219
Hassan Amouhzi <github@anezi.net>
2013-10-01 17:25:43 +0200

    Update virtualbox.sh

--- install/virtualbox.sh
+++ install/virtualbox.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-cd /tmp
-
-sudo apt-get install virtualbox
+sudo apt-get install virtualbox virtualbox-guest-additions-iso
 
 sudo usermod -G vboxusers -a $USER
 
-version=$(VBoxManage --version|cut -dr -f1|cut -d'_' -f1) && wget -c http://download.virtualbox.org/virtualbox/$version/Oracle_VM_VirtualBox_Extension_Pack-$version.vbox-extpack
+cd /tmp
 
-version=$(VBoxManage --version|cut -dr -f1|cut -d'_' -f1) && VBoxManage extpack install Oracle_VM_VirtualBox_Extension_Pack-$version.vbox-extpack
+version=$(VBoxManage --version|cut -dr -f1|cut -d'_' -f1)
+
+wget -c http://download.virtualbox.org/virtualbox/$version/Oracle_VM_VirtualBox_Extension_Pack-$version.vbox-extpack
+
+VBoxManage extpack install Oracle_VM_VirtualBox_Extension_Pack-$version.vbox-extpack
 
 rm *.vbox-extpack
