99f262052f8e952d301cc7f53d857cdfcd73051b
Hassan Amouhzi <github@anezi.net>
2013-10-02 13:05:14 +0200

    Update fdk-aac.sh

--- install/fdk-aac.sh
+++ install/fdk-aac.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
 cd /tmp
-git clone --depth 1 git://github.com/mstorsjo/fdk-aac.git
+mkdir fdk-aac
 cd fdk-aac
+wget https://github.com/mstorsjo/fdk-aac/archive/v0.1.2.zip
+unzip v0.1.2.zip
+cd fdk-aac-0.1.2
 autoreconf -fiv
 ./configure
 make
-make install
+sudo make install
 make distclean
+cd ..
+cd ..
+rm -r fdk-aac
