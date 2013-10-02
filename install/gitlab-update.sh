38f08cc24d4e3aaea714b3096b11d33223542213
Hassan Amouhzi <github@anezi.net>
2013-10-02 06:26:22 +0200

    Update gitlab-update.sh

--- install/gitlab-update.sh
+++ install/gitlab-update.sh
@@ -8,12 +8,23 @@ sudo -u git -H RAILS_ENV=production bundle exec rake gitlab:backup:create
 read -p "Continue? Y/n" res
 res=${res:-Y}
 
-if test res ! = "Y" then
+if test res ! = "Y"
+then
   echo "Exit."
   exit
 fi
 
+echo "Stop the gitlab server:"
 sudo service gitlab stop
+
+read -p "Continue? Y/n" res
+res=${res:-Y}
+
+if test res ! = "Y"
+then
+  echo "Exit."
+  exit
+fi
 
 sudo -u git -H git pull
 
