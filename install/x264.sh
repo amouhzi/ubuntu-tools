77adf863bfb8547ff7d45e0c5cbbc570fe5126b9
Hassan Amouhzi <github@anezi.net>
2013-10-02 19:29:49 +0200

    Update x264.sh

--- install/x264.sh
+++ install/x264.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-sudo apt-get install -y x264
+WUSER="www-data"
+WHOME="/var/www"
+
+cd $WHOME
+sudo -u $WUSER -H git clone --depth 1 git://git.videolan.org/x264.git
+
+cd x264
+sudo -u $WUSER -H ./configure --prefix="$WHOME/ffmpeg_build" --bindir="$WHOME/bin" --enable-static
+sudo -u $WUSER -H make
+sudo -u $WUSER -H make install
+sudo -u $WUSER -H make distclean
