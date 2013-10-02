d342d8909f6ec4f8b1d2af829c15718d6b582ff3
Hassan Amouhzi <github@anezi.net>
2013-10-02 06:23:16 +0200

    Update aget-update.sh

--- install/aget-update.sh
+++ install/aget-update.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
-sudo wget sh.anezi.net/aget -O /usr/local/bin/aget
-sudo chmod +x /usr/local/bin/aget
+echo -n "Download of aget script ... "
+sudo wget -q sh.anezi.net/aget -O /usr/local/bin/aget
+
+if [ -f /usr/local/bin/aget ]
+then
+    echo "OK."
+    sudo chmod +x /usr/local/bin/aget
+    echo "You can now use aget to install or update applications."
+else
+    echo "ERROR."
+fi
