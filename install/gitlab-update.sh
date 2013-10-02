735ce682519a7b40fc1e5dabdf80c986839698c8
Hassan Amouhzi <github@anezi.net>
2013-10-02 07:02:07 +0200

    Update gitlab-update.sh

--- install/gitlab-update.sh
+++ install/gitlab-update.sh
@@ -6,8 +6,8 @@ ask() {
     then
             return 1;
     else
-            exit
             echo "Abort.."
+            exit
     fi
 }
 
@@ -23,15 +23,38 @@ sudo service gitlab stop
 
 ask "Continue? [y/N] "
 
+echo "Get latest code"
+
 sudo -u git -H git pull
 
-sudo -u git -H bundle exec rake assets:clean RAILS_ENV=production
-sudo -u git -H bundle exec rake assets:precompile RAILS_ENV=production
-sudo -u git -H bundle exec rake cache:clear RAILS_ENV=production
+read -p "Need to recompile assets? [y/N] " REPLY
+
+if test $REPLY = "y"
+then
+
+    sudo -u git -H bundle exec rake assets:clean RAILS_ENV=production
+    sudo -u git -H bundle exec rake assets:precompile RAILS_ENV=production
+    sudo -u git -H bundle exec rake cache:clear RAILS_ENV=production
+    
+fi
+
+echo "Start gitlab server .. "
 
 sudo service gitlab start
+
+echo "Restart nginx .. "
+
 sudo service nginx restart
+
+echo "Info: "
 
 sudo -u git -H bundle exec rake gitlab:env:info RAILS_ENV=production
 
-sudo -u git -H bundle exec rake gitlab:check RAILS_ENV=production
+read -p "Need to check the environment? [y/N] " REPLY
+
+if test $REPLY = "y"
+then
+    sudo -u git -H bundle exec rake gitlab:check RAILS_ENV=production
+fi
+
+echo "Finish! "
