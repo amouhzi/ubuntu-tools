7d7157964b7102126be1ec710897ea18f86f538e
Hassan Amouhzi <github@anezi.net>
2013-10-02 06:07:55 +0200

    Update aget-install.sh

--- install/aget-install.sh
+++ install/aget-install.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-echo "Download of aget script ... "
+echo -n "Download of aget script ... "
 sudo wget -q sh.anezi.net/aget -O /usr/local/bin/aget
 
 if [ -f /usr/local/bin/aget ]
 then
     echo "OK."
+    sudo chmod +x /usr/local/bin/aget
 else
     echo "ERROR."
 fi
-
-sudo chmod +x /usr/local/bin/aget
