b4abe6e375d44edc606939ea2cbc50593c35b15e
Hassan Amouhzi <github@anezi.net>
2013-10-06 16:06:26 +0200

    Update gitlab-update.sh

--- install/gitlab-update.sh
+++ install/gitlab-update.sh
@@ -13,45 +13,38 @@ ask() {
 
 cd /home/git/gitlab
 
-echo "Doing a backup, please wait ... "
-sudo -u git -H RAILS_ENV=production bundle exec rake gitlab:backup:create
-
-ask "Continue? [y/N] "
-
-echo "Stop the gitlab server:"
-sudo service gitlab stop
+# BACKUP
+read -p "Need a backup? [y/N] " REPLY
+if test $REPLY = "y"
+then
+    echo "Doing a backup, please wait ... "
+    sudo -u git -H RAILS_ENV=production bundle exec rake gitlab:backup:create
+fi
 
 ask "Continue? [y/N] "
 
 echo "Get latest code"
-
 sudo -u git -H git pull
 
 read -p "Need to recompile assets? [y/N] " REPLY
-
 if test $REPLY = "y"
 then
-
     sudo -u git -H bundle exec rake assets:clean RAILS_ENV=production
     sudo -u git -H bundle exec rake assets:precompile RAILS_ENV=production
     sudo -u git -H bundle exec rake cache:clear RAILS_ENV=production
-    
 fi
 
-echo "Start gitlab server .. "
+echo "Restart gitlab server .. "
+sudo service gitlab restart
 
-sudo service gitlab start
-
-echo "Restart nginx .. "
-
-sudo service nginx restart
-
-echo "Getting environment informations, please wait .. "
-
-sudo -u git -H bundle exec rake gitlab:env:info RAILS_ENV=production
+read -p "Getting environment informations? [y/N] " REPLY
+if test $REPLY = "y"
+then
+    echo "Getting environment informations, please wait .. "
+    sudo -u git -H bundle exec rake gitlab:env:info RAILS_ENV=production
+fi
 
 read -p "Need to check the environment? [y/N] " REPLY
-
 if test $REPLY = "y"
 then
     sudo -u git -H bundle exec rake gitlab:check RAILS_ENV=production
