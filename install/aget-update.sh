9868fb81f8e36844b8dbdb02a5a46a41fcc063fb
Hassan Amouhzi <github@anezi.net>
2013-10-09 00:09:56 +0200

    Update aget-update.sh

--- install/aget-update.sh
+++ install/aget-update.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+version=0.5
+vdate=2013-10-08
+
+echo
+echo "Anezi Get Updater version $version date $vdate."
+echo
+
 echo -n "Download of aget script ... "
 sudo wget -q sh.anezi.net/aget -O /usr/local/bin/aget
 
