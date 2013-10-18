148431d37546fb383d60d2cf7864e5457b00b5c0
Hassan Amouhzi <github@anezi.net>
2013-10-18 18:06:50 +0200

    Update teamviewer7-amd64.sh

--- install/teamviewer7-amd64.sh
+++ install/teamviewer7-amd64.sh
@@ -6,7 +6,7 @@ echo "\nDownloading of teamviewer 7:"
 wget "http://download.teamviewer.com/download/version_7x/teamviewer_linux_x64.deb"
 
 echo "\nInstallation of teamviewer 7:"
-dpkg -i teamviewer_linux_x64.deb
+sudo dpkg -i teamviewer_linux_x64.deb
 
 echo "\nInstallation of missing packages:"
-apt-get install -fy
+sudo apt-get install -fy
