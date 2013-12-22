b0bc21b0925ddc1b71dcf4f7d45c1848c7426bbd
Hassan Amouhzi <github@anezi.net>
2013-12-22 17:18:31 +0100

    Update teamviewer-amd64.sh

--- install/teamviewer-amd64.sh
+++ install/teamviewer-amd64.sh
@@ -2,6 +2,12 @@
 
 cd /tmp
 
+wget "http://launchpadlibrarian.net/139194357/lib32asound2_1.0.25-4ubuntu4_amd64.deb"
+
+sudo dpkg -i lib32asound2_1.0.25-4ubuntu4_amd64.deb
+
+sudo apt-get install -fy
+
 wget "http://download.teamviewer.com/download/teamviewer_linux_x64.deb"
 
 sudo dpkg -i teamviewer_linux_x64.deb
