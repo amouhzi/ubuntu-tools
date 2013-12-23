d15b6ea5feff3ed4dffaa8ea3b60afa1309a7991
Hassan Amouhzi <github@anezi.net>
2013-12-23 01:09:38 +0100

    Update codecs-amd64.sh

--- install/codecs-amd64.sh
+++ install/codecs-amd64.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 sudo apt-get -y install gstreamer0.10-ffmpeg gstreamer0.10-plugins-bad gstreamer0.10-plugins-bad-multiverse \
-gstreamer0.10-plugins-ugly gstreamer0.10-plugins-ugly-multiverse
+gstreamer0.10-plugins-ugly
