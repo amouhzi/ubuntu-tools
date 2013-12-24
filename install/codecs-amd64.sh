42c56635a632c93f12035451e9235112e1845625
Hassan Amouhzi <github@anezi.net>
2013-12-25 00:05:21 +0100

    Update codecs-amd64.sh

--- install/codecs-amd64.sh
+++ install/codecs-amd64.sh
@@ -2,3 +2,6 @@
 
 sudo apt-get -y install gstreamer0.10-ffmpeg gstreamer0.10-plugins-bad gstreamer0.10-plugins-bad-multiverse \
 gstreamer0.10-plugins-ugly
+
+
+sudo apt-get -y install ubuntu-restricted-extras
