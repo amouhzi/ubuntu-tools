717d516136639d2a1a4e86efc3bb43c35a0c9c71
Hassan Amouhzi <github@anezi.net>
2014-01-01 18:23:18 +0100

    Update webmin.sh

--- install/webmin.sh
+++ install/webmin.sh
@@ -9,7 +9,6 @@ cd /tmp
 wget http://www.webmin.com/jcameron-key.asc
 cat jcameron-key.asc | sudo apt-key add -
 
-sudo -s
 echo "deb http://download.webmin.com/download/repository sarge contrib" > /etc/apt/sources.list.d/webmin.list
 
 # Updating packages informations
